c62e42b9aef1bed1b81cb30024d922207432bad4
Dr. XyClo <sdcarmody@gmail.com>
2019-11-18 02:51:04 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,4 +1,17 @@
 class Course
+  attr_accessor :title, :schedule, :description
+  
+  @@course_class = []
+  
+  def initialize(title, schedule, description)
+    @title = title
+    @schedule = schedule
+    @description = description
+  end
+  
+  def self.all 
+    @@course_class
+  end
   
 end
 
