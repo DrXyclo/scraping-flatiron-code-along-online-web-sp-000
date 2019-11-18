f7d926cf5de8e9b9a1bdb2aed57ec70ab87462a9
Dr. XyClo <sdcarmody@gmail.com>
2019-11-18 03:07:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,8 @@ class Scraper
   def get_page
     
     html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
-    doc = Nokogiri::HTML(html)
+   # doc = 
+   Nokogiri::HTML(html)
     # doc
   end
   
